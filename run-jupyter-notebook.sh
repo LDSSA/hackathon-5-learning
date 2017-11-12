7d69d58f86fe798960c7b79d0f42892508a00e47
Samuel Hopkins <sam@crowdprocess.com>
2017-11-12 12:01:59 +0000

    version 2 with less datasets

--- run-jupyter-notebook.sh
+++ run-jupyter-notebook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run  --volume `pwd`:/root/workdir --workdir /root/workdir -it --rm -p 127.0.0.1:8888:8888 ldssa/hackathon-5:1
+docker run  --volume `pwd`:/root/workdir --workdir /root/workdir -it --rm -p 127.0.0.1:8888:8888 ldssa/hackathon-5:2
 
